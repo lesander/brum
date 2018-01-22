4092fe521e76cc643189e696ed4d9c73b28d561a
BrumPi <35688397+BrumPi@users.noreply.github.com>
2018-01-22 14:17:54 +0100

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -7,3 +7,6 @@ python /home/brum/repo/webhook.py >> /home/brum/repo/all.log 2>&1 &
 
 # Start the ultrahook forwarder.
 ultrahook -k 'DSOSXGcVFi8Mh1Fx7Fv6vn6Dg73JXQns' github 5000 >> /home/brum/repo/all.log 2>&1 &
+
+# Start Brum!
+python /home/brum/repo/start.py >> /home/brum/repo/all.log 2>&1 &
