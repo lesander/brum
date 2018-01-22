2a029e980cd94abd394d9b175e0682b8f29d88f1
BrumPi <35688397+BrumPi@users.noreply.github.com>
2018-01-22 15:24:32 +0100

    Update start.py

--- start.py
+++ start.py
@@ -3,7 +3,7 @@ import RPi.GPIO as GPIO
 import time
 print 'Starting BRUM now. :)'
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(32, GPIO.IN)
+GPIO.setup(32, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
   print GPIO.input(32)
