a100e5350ef5a761f6aa1635ec2fa128454e85ee
BrumPi <35688397+BrumPi@users.noreply.github.com>
2018-01-22 12:48:13 +0100

    Update and rename webserver.py to webhook.py

--- webhook.py
+++ webhook.py
@@ -1,4 +1,7 @@
 import socket
+from subprocess import call
+
+import config
 
 HOST, PORT = 'localhost', 5000
 
@@ -6,7 +9,9 @@ listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 listen_socket.bind((HOST, PORT))
 listen_socket.listen(1)
+
 print 'Serving HTTP on port %s ...' % PORT
+
 while True:
     client_connection, client_address = listen_socket.accept()
     request = client_connection.recv(1024)
@@ -15,7 +20,10 @@ while True:
     http_response = """\
 HTTP/1.1 200 OK
 
-Hello, World!
+Starting update procedure.
 """
     client_connection.sendall(http_response)
     client_connection.close()
+    
+    call(["git", "pull", "https://brumpi:"+config.token()+"@github.com/lesander/brum.git"])
+    
