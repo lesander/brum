eefb8956945cb2a6f8024893023c39ba6d1314cc
Sander Laarhoven <sander@laarhoven.org>
2018-01-25 21:46:45 +0100

    Add some documentation to JS code.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,19 @@
+/**
+ * BRUM v1.0.0
+ * By Bas, Sander & Tian
+ * Copyright (c) 2018 All Rights Reserved.
+ * https://github.com/lesander/brum
+ */
+
 const cardImages = $(`.card > img`)
 const submitBtn = $(`.submit > button`)
 
+/**
+ * Select or deselect the clicked card on click
+ *  and enable or disable the submit button.
+ * @param  {Element} cardImages
+ * @return {void}
+ */
 $(cardImages).on(`click`, (event) => {
 
   const card = $(event.target).parent()
@@ -16,13 +29,27 @@ $(cardImages).on(`click`, (event) => {
 
 })
 
+/**
+ * Submit the chosen destination on click of the submit button.
+ * @param  {Element} submitBtn
+ * @return {void}
+ */
 $(submitBtn).on(`click`, (event) => {
 
   const value = $(`.card.selected`).attr(`data-location`)
+
   $.post(`http://github.brum.ultrahook.com/dawae/${value}`, (response) => {
-    console.log(response)
+
+    // Handle any error(s).
+    if (response.statusCode !== 200) {
+      alert('Er ging iets mis met het versturen van de keuze. Probeer het nog een keer.')
+      console.log(response)
+    }
+
+    // TODO: Display the progress screen.
+    //       Initialize polling for finished status.
+    
+
   })
 
-  // TODO: Display progress screen.
-  //       Initialize polling for finish.
 })
