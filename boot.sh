4479c306712724d8481c01d2e134807349f688b1
Sander Laarhoven <sander@laarhoven.org>
2018-01-25 22:43:18 +0100

    Do not start BRUM on system boot.

--- boot.sh
+++ boot.sh
@@ -9,4 +9,4 @@ python /home/brum/repo/webhook.py >> /home/brum/repo/all.log 2>&1 &
 ultrahook -k 'DSOSXGcVFi8Mh1Fx7Fv6vn6Dg73JXQns' github 5000 >> /home/brum/repo/all.log 2>&1 &
 
 # Start Brum!
-python /home/brum/repo/start.py >> /home/brum/repo/all.log 2>&1 &
+#python /home/brum/repo/start.py >> /home/brum/repo/all.log 2>&1 &
